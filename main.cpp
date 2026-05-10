17b028ddec266ef93dbeda343450d67bba1fe6f8
justinguto <jutogustowski@gmail.com>
2026-05-10 11:07:30 -0700

    Fix bubble sort functions

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
-#include "main.hpp"
+#include <iostream>
+#include <random>
+#include <iomanip>
+using namespace std;
 
-int main()
+const int SIZE = 100;
+
+int makearray(int[]);
+void bubble(int[], int);
+void printout(int[], int);
+
+int makearray(int number[])
 {
-	int number[SIZE];
-	int last;
+    random_device rd;
+    mt19937 gen(rd());
 
-	last = makearray(number);
-	printout(number, last);
-	bubble(number, last);
-	printout(number, last);
-	// for (int i = 0; i < last; i++)
-	// 	bubble(number, last);
-	// printout(number, last);
+    uniform_int_distribution<int> sizeDist(10, 20);
+    uniform_int_distribution<int> valueDist(0, 99);
+
+    int count = sizeDist(gen);
+
+    for (int i = 0; i < count; i++)
+    {
+        number[i] = valueDist(gen);
+    }
+
+    return count;
+}
+
+void bubble(int number[], int last)
+{
+    for (int i = 0; i < last - 1; i++)
+    {
+        if (number[i] > number[i + 1])
+        {
+            int temp = number[i];
+            number[i] = number[i + 1];
+            number[i + 1] = temp;
+        }
+    }
+}
+
+void printout(int number[], int last)
+{
+    for (int i = 0; i < last; i++)
+    {
+        cout << number[i] << "\t";
+    }
+    cout << endl;
 }
--- main.hpp
+++ main.hpp
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <random>
 #include <iomanip>
 using namespace std;
 
-int makearray(int[]);
-void bubble(int[], int);
-void printout(int[], int);
-const int SIZE = 100;
-
 int makearray(int number[])
 {
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<int> sizeDist(10, 20);
+    uniform_int_distribution<int> valueDist(0, 99);
+
+    int count = sizeDist(gen);
+
+    for (int i = 0; i < count; i++)
+    {
+        number[i] = valueDist(gen);
+    }
+
+    return count;
 }
+
 void bubble(int number[], int last)
 {
+    for (int i = 0; i < last - 1; i++)
+    {
+        if (number[i] > number[i + 1])
+        {
+            int temp = number[i];
+            number[i] = number[i + 1];
+            number[i + 1] = temp;
+        }
+    }
 }
+
 void printout(int number[], int last)
 {
+    for (int i = 0; i < last; i++)
+    {
+        cout << number[i] << "\t";
+    }
+
+    cout << endl;
 }
